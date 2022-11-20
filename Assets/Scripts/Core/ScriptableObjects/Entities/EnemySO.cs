c1f9c46815c4a879e4dda21ba43c4facb0cd8f09
NixonMSilva <nixonmoreira42@gmail.com>
2022-11-19 22:58:38 -0300

    Entity Scriptable Objects implemented, created template asset for both

---
+++ Assets/Scripts/Core/ScriptableObjects/Entities/EnemySO.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Core.ScriptableObjects.Entities
+{
+    [CreateAssetMenu(fileName = "New Enemy", menuName = "Entity/Enemy")]
+    public class EnemySO : EntitySO
+    {
+        [Header("Combat Information")]
+        [Space]
+        public float defeatXP;
+        public float sightRange;
+
+        /* Itens
+         public Items[] drops;
+         public Behavior behavior;
+         */
+    }
+}
