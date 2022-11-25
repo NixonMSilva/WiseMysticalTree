2f703cadef89bccff43ab54da1e0c3004042fef4
basilisco09 <105323687+basilisco09@users.noreply.github.com>
2022-11-25 14:14:35 -0300

    Scripts contendo as classes de itens consumíveis e equipamentos.

---
+++ Assets/Scripts/Core/Items/Item.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Items
+{
+    public class Item : MonoBehaviour
+
+    {
+
+        protected int price;
+        protected int level;
+        protected float magnitude;
+
+        public int GetPrice()
+        {
+            return price;
+        }
+
+        private void Equip()
+        {
+
+        }
+
+        private void Desquip()
+        {
+
+        }
+
+        private void Discard()
+        {
+
+        }
+
+        private void Sell()
+        {
+
+        }
+        void Start()
+        {
+
+        }
+        void Update()
+        {
+
+        }
+    }
+}
